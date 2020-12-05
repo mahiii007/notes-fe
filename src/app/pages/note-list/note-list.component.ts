6046954520ea7c33708310059822a438a2e160eb
Satyajit <satyajit.m@exto360.com>
2020-12-05 20:19:33 +0530

    Added filter feature

--- src/app/pages/note-list/note-list.component.ts
+++ src/app/pages/note-list/note-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ɵQueryValueType } from "@angular/core";
 import { Note } from "src/app/shared/notes.model";
 import { NotesService } from "src/app/shared/notes.service";
 
@@ -9,12 +9,49 @@ import { NotesService } from "src/app/shared/notes.service";
 })
 export class NoteListComponent implements OnInit {
   notes: Note[] = new Array<Note>();
+  filteredNotes: Note[] = new Array<Note>();
   constructor(private noteService: NotesService) {}
 
   ngOnInit() {
     this.notes = this.noteService.getAll();
+    this.filteredNotes = this.notes;
   }
   deleteNote(id: number) {
     this.noteService.delete(id);
   }
+  filter(query: string){
+    console.log(query);
+    let allResults: Note[] = new Array<Note>();
+    query = query.toLowerCase().trim();
+    let words: string[] = query.split(' ');//splits into words when there is a space
+    words = this.removeDuplicates(words);
+    words.forEach(term=>{
+      let results: Note[] = this.relevantNotes(term);
+      allResults = [...allResults, ...results];
+    })
+    let uniqueResult = this.removeDuplicates(allResults);
+     this.filteredNotes = uniqueResult;
+  }
+  removeDuplicates(arr: Array<any>): Array<any>{
+    let uniqueResults: Set<any> = new Set<any>();
+    arr.forEach(item=>{
+      uniqueResults.add(item);
+    })
+   return Array.from(uniqueResults);
+
+  }
+  relevantNotes(query: string): Array<any>{
+     query = query.toLowerCase().trim();
+     let relevantNotes = this.notes.filter(note =>{
+       if(note.title && note.title.toLowerCase().includes(query)){
+         return true;
+       }
+       if(note.body && note.body.toLowerCase().includes(query)){
+        return true;
+      }
+      return false;
+     })
+     return relevantNotes;
+  }
 }
+
